015a037f282b55751f9397b3e0cf07bfaa36a8f4
maisunmonowar <maisunmonwar@gmail.com>
2015-04-20 11:20:34 +0600

    Update riverLine.cpp

--- riverLine/src/riverLine.cpp
+++ riverLine/src/riverLine.cpp
@@ -17,12 +17,14 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
+	//to read an image file. Use following lines
 	Mat img = imread(argv[1]);
 	            if (img.empty())
 	            {
 	                cout << "Can't read image '" << argv[1] << "'\n";
 	                return -1;
 	            }
+//if source is from video. use following three lines
     //VideoCapture cap(0); // open the default camera
     //if(!cap.isOpened())  // check if we succeeded
       //  return -1;
@@ -30,22 +32,22 @@ int main(int argc, char** argv)
     Mat edges;
     namedWindow("original",WINDOW_AUTOSIZE);
     namedWindow("first",WINDOW_AUTOSIZE);//WINDOW_AUTOSIZE
-    namedWindow("second",WINDOW_AUTOSIZE);
-    for(;;)
-    {
+    namedWindow("result",WINDOW_AUTOSIZE);
+   //if source is video, use loop
+    //for(;;)
+    //{
        // Mat frame;
         //cap >> frame; // get a new frame from camera
         cvtColor(img, edges, CV_BGR2GRAY);
         imshow("original", edges);
         GaussianBlur(edges, edges, Size(9,9), 10, 10);
-
         GaussianBlur(edges, edges, Size(9,9), 10, 10);
-        imshow("second", edges);
+        //imshow("second", edges);//debug purpose
         Canny(edges, edges, 10, 30, 3);
-        imshow("first", edges);
+        imshow("result", edges);
 
-        if(waitKey(30) >= 0) break;
-    }
+    //    if(waitKey(30) >= 0) break;
+    //}
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
