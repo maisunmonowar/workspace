ed073bb6d93c9dcfcf13db4916ac4a4e44ac3e8e
maisunmonowar <maisunmonwar@gmail.com>
2015-04-22 20:52:31 +0600

    Update green_percentage_T2.cpp

--- green_percentage_T2/src/green_percentage_T2.cpp
+++ green_percentage_T2/src/green_percentage_T2.cpp
@@ -1,27 +1,42 @@
 //============================================================================
 // Name        : green_percentage_T2.cpp
 // Author      : Maisun Monowar
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Version     : 02
+// Copyright   : GPL V.2
+// Description : This program analyzes video from default source and calculates
+//		crop health and makes appropiate sugesstion to user.
 //============================================================================
 
-#include <opencv2/opencv.hpp>
+
+
+//including required files to compile this program
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 
 
-using namespace std;
+
+//Using Standard Name Space
+using namespace std; 
+
+//Using OpenCV Namespace
 using namespace cv;
 
-//defining 'green'
+
+
+//Defining 'green' color in HSV color space
+
 #define green_h_h 80
 #define green_h_l 45
 #define green_s_h 255
 #define green_s_l 30
 #define green_v_h 200
 #define green_v_l 30
+
+
+
+//Defining "Healthy Green" Color in HSV color space
 
 #define healthy_h_h 65
 #define healthy_h_l 55
@@ -30,57 +45,81 @@ using namespace cv;
 #define healthy_v_h 200
 #define healthy_v_l 30
 
-// comment to test git
+
+
+//Main Function
 
 int main(int, char**)
 {
-	Mat img_hsv, imgThresholded, healthyThreshold;
-		int total_pixel = 0;
-		int total_green = 0;
-		int healthy_green = 0;
-		float percentage_green = 0;
-		float percentage_healthy_green ;
+	//Initializing Variables to use in the program
+	
+	Mat img_hsv;
+	Mat imgThresholded;
+	Mat healthyThreshold;
+	Mat edges;
+	int total_pixel = 0;
+	int total_green = 0;
+	int healthy_green = 0;
+	float percentage_green = 0;
+	float percentage_healthy_green ;
+	
+	
+	
+	// open the default camera
+    	VideoCapture cap(0); 
+    	
+    	
+    	// check if we succeeded
+    	if(!cap.isOpened())  
+        	return -1;
 
-    VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
-
-    Mat edges;
-    namedWindow("edges",1);
-
+    	//Creating windows to display results
+    	namedWindow("edges",1);
 	namedWindow( "Original Image", WINDOW_AUTOSIZE );
 	namedWindow( "Thresholded Image", WINDOW_AUTOSIZE );
 	namedWindow( "Healthy Thresholded Image", WINDOW_AUTOSIZE );
 
-    for(;;)
-    {
-        Mat image;
-        cap >> image; // get a new frame from camera
-        total_pixel = image.total();
+    	//Running an infinite loop to process incoming videos
+    	for(;;)
+    	{
+        	//Create a Temporary Matrix to hols image
+        	Mat image;
+        	// get a new frame from camera
+        	cap >> image; 
+        	total_pixel = image.total();
+        	//Convert image to HSV color space
         	cvtColor(image, img_hsv, COLOR_BGR2HSV);
+        	//Threshholding image to display
         	inRange(img_hsv, Scalar(green_h_l, green_s_l, green_v_l), Scalar(green_h_h, green_s_h, green_v_h), imgThresholded); //Threshold the image
         	inRange(img_hsv, Scalar(healthy_h_l, healthy_s_l, healthy_v_l), Scalar(healthy_h_h, healthy_s_h, healthy_v_h), healthyThreshold);
+        	
+        	
+        	//Following block of code is credited to
         	//http://stackoverflow.com/questions/7899108/opencv-get-pixel-channel-value-from-mat-image
         	for(int i = 0; i < img_hsv.rows; i++)
         	{
         		for(int j = 0; j < img_hsv.cols; j++)
         		{
         			Vec3b hsvPixel = img_hsv.at<Vec3b>(i, j);
-        			// do something with BGR values...
+        			// Compare Each pixel
         			if (hsvPixel.val[1] > green_s_l)
         			{
         				if (hsvPixel.val[2]>green_v_l && hsvPixel.val[2]<green_v_h)
         				{
         					if (hsvPixel.val[0]>green_h_l && hsvPixel.val[0]<green_h_h)
         					{
+        						//this Pixel is a "Green" pixel as defined above
         						total_green++;
-        						//time to fine tune
+        						
+        						
+        						//Searching for "Healthy Green"
         						if (hsvPixel.val[1] > healthy_s_l)
         						{
         							if (hsvPixel.val[2]>healthy_v_l && hsvPixel.val[2]<healthy_v_h)
         							{
         								if (hsvPixel.val[0]>healthy_h_l && hsvPixel.val[0]<healthy_h_h)
         								{
+        									//This Pixel is "Healthy Green"
         									healthy_green++;
         								}
         							}
@@ -90,20 +129,28 @@ int main(int, char**)
         			}
         		}
         	}
+        	//Pixel comparision complete
        	
+       		//Calculating Percentage of Green Pixel
         	percentage_green = total_green * 100.0 / total_pixel;
+        	
+        	//Calculating Percentage of Healthy Green Pixel
         	percentage_healthy_green = healthy_green * 100.0/ total_green ;
 
-        	//display graphic
-
+        	
+        	
+        	//display the result on window
         	imshow( "Original Image", image );
         	imshow( "Thresholded Image", imgThresholded );
-      //  	imwrite("imgThresholded.jpg", imgThresholded);
-
         	imshow( "Healthy Thresholded Image", healthyThreshold );
-    //    	imwrite("healthyThreshold.jpg", healthyThreshold);
+	
+		//Save result as image if required 
+	 	//imwrite("imgThresholded.jpg", imgThresholded);
+    		//imwrite("healthyThreshold.jpg", healthyThreshold);
 
-        	//state calculation data
+        	
+        	
+        	//state calculation data to user
         	std::cout <<"Total Pixel ";
         	std::cout <<total_pixel;
 
